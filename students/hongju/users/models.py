467efbbe95a1c10180824a956f75054a8e21b454
hjk0795 <hjk0795@gmail.com>
2021-12-17 14:49:13 +0900

    [김홍주] Mission 2: 모델링

---
+++ students/hongju/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name            = models.CharField(max_length=45)
+    email           = models.CharField(max_length=300)
+    password        = models.CharField(max_length=45)
+    phone_number    = models.CharField(max_length=45)
+    additional_info = models.TextField()
+
+    class Meta:
+        db_table = "user"
