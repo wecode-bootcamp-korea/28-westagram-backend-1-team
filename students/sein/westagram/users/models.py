11b384b87c022081d7609d4feb665c770223890e
seinthemag <seinthemag@gmail.com>
2021-12-17 16:53:50 +0900

    FIX: users app models.py

--- students/sein/westagram/users/models.py
+++ students/sein/westagram/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    user_name     = models.CharField(max_length=30)
+    name          = models.CharField(max_length=30)
     email         = models.EmailField(max_length=100, unique=True)
     password      = models.CharField(max_length=300)
     mobile_number = models.CharField(max_length=20, unique=True)
-    other_info    = models.CharField(max_length=100, blank=True)
+    created_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "users"
