54ad35003cc9b0b75932d81ccea5170efc60e631
seinthemag <seinthemag@gmail.com>
2021-12-20 20:44:59 +0900

    ADD: Sign up views & urls

--- students/sein/westagram/users/views.py
+++ students/sein/westagram/users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email    = data['email']
+            password = data['password']
+
+            if User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'message' : 'ALREADY_EXIST'}, status = 400)
+
+            email_regex    = '[a-zA-Z0-9.-_+]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+'
+            password_regex = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
+            if not re.match(email_regex, data['email']):
+                return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
+
+            if not re.match(password_regex, data['password']):
+                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
+       
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)    
+
+        else:
+            User.objects.create(
+                email         = data['email'],
+                password      = data['password'],
+                name          = data['name'],
+                mobile_number = data['mobile_number']
+                )
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+
+            
