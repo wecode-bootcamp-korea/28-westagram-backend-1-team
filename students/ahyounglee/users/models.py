753c162b1a98e8bba953ff3418c1ff125b9f2745
AhYoung Lee <dkdud5135@naver.com>
2021-12-20 11:59:40 +0900

    Modify : models.py 수정(phone -> phone_number 변수명 수정, 필드 옵션 수정)

--- students/ahyounglee/users/models.py
+++ students/ahyounglee/users/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.core.validators import RegexValidator
-
 
 class User(models.Model):
-    username         = models.CharField(max_length=45, blank=False, null=False)
-    email            = models.EmailField(max_length=100, blank=False, null=False)
-    passWord         = models.CharField(max_length=256, blank=False, null=False)
-    phone            = models.CharField(max_length = 11, unique = True)
+    username         = models.CharField(max_length=45)
+    email            = models.EmailField(max_length=100)
+    passWord         = models.CharField(max_length=256)
+    phone_number     = models.CharField(max_length = 40, unique = True)
     is_professional  = models.BooleanField(default=False)
     created_at       = models.DateTimeField(auto_now_add=True)
     updated_at       = models.DateTimeField(auto_now=True)
