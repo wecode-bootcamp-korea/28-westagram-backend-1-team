0564a4811073122a11d9954c93e935999a0a8eef
seinthemag <seinthemag@gmail.com>
2021-12-21 16:13:58 +0900

    ADD: Login view

--- students/sein/westagram/users/urls.py
+++ students/sein/westagram/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from users.views import SignUpView
+from users.views import SignUpView, LogInView
 
 urlpatterns = [
-    path('/signup', SignUpView.as_view())
+    path('/signup', SignUpView.as_view()),
+    path('/login', LogInView.as_view())
 ]
+
 
--- students/sein/westagram/users/views.py
+++ students/sein/westagram/users/views.py
@@ -34,3 +34,22 @@ class SignUpView(View):
             return JsonResponse({'message' : 'SUCCESS'}, status = 201)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+class LogInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body) 
+
+            email    = data['email']
+            password = data['password']
+          
+            if not User.objects.filter(email=data['email']).exists() \
+               or not User.objects.filter(password=data['password']).exists():
+                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+
+            if User.objects.get(email=data['email']).password != password:
+                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+
+            return JsonResponse({'message' : 'SUCCESS'}, status = 200)            
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
