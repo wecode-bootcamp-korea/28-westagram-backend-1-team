95fcc9b4749a824e4b31b006e5aed4bb50d5c810
MinWook Jang <jmw1080@gmail.com>
2021-12-21 14:08:48 +0900

    project signup

--- students/minwookjang/users/models.py
+++ students/minwookjang/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name               = models.CharField(max_length=20)
     email              = models.EmailField(max_length=128, unique= True)
     password           = models.CharField(max_length=300)
-    phone_number       = models.CharField(max_length = 11, unique = True)
+    phone_number       = models.CharField(max_length = 20, unique = True)
     date_of_birth      = models.DateField(null = True)
     created_at         = models.DateTimeField(auto_now_add=True)
     updated_at         = models.DateTimeField(auto_now=True)
