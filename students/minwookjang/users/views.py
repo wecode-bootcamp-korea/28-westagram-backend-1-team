f06451f4bb53a21950e457ff37b5fdf0e6e70391
MinWook Jang <jmw1080@gmail.com>
2021-12-21 16:03:25 +0900

    project signup modify

--- students/minwookjang/users/views.py
+++ students/minwookjang/users/views.py
@@ -7,16 +7,15 @@ from django.http    import JsonResponse
 from django.views   import View
 from users.models   import User
 
-
-## 이메일 조건 검사
 def validate_email(email):
     pattern = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    
     if not re.match(pattern,email):
         raise ValidationError('EMAIL_VALIDATION')
 
-## 패스워드 조건 검사
 def validate_password(password):
     pattern = '^[0-9a-z~!@#$%^&*()_+A-Z]*.{8,}$'
+    
     if not re.match(pattern,password):
         raise ValidationError('PASSWORD_VALIDATION')
 
@@ -25,23 +24,12 @@ class SignupView(View):
         data = json.loads(request.body)
 
         try:
-            if not data['email'] or not data['password']:
-                return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
-
-            # 이메일 중복 여부 검사
-            elif User.objects.filter(email = data['email']):
+            if User.objects.filter(email = data['email']):
                 return JsonResponse({'message' : 'EMAIL_DUPLICATE_VALUES'}, status = 400)
 
             validate_email(data['email'])
             validate_password(data['password'])
 
-        except KeyError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
-
-        except ValidationError as e:
-            return JsonResponse({'message':e.message}, status=400)
-
-        else:
             User.objects.create(
                 name          = data['name'],
                 email         = data['email'],
@@ -50,3 +38,9 @@ class SignupView(View):
                 date_of_birth = data['date_of_birth']
             )
             return JsonResponse({'message': 'CREATED'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+        except ValidationError as e:
+            return JsonResponse({'message':e.message}, status=400)
