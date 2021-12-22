ce684937988288ce214931baa6cf2ad961800835
hjk0795 <hjk0795@gmail.com>
2021-12-22 17:54:42 +0900

    [김홍주] Mission 3 수정3

--- students/hongju/users/models.py
+++ students/hongju/users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    password     = models.CharField(max_length=255)
     name         = models.CharField(max_length=45)
-    email        = models.CharField(max_length=300)
-    password     = models.CharField(max_length=45)
+    email        = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=45)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
--- students/hongju/users/views.py
+++ students/hongju/users/views.py
@@ -1,10 +1,11 @@
-import json
 import re
+import json
 
-from django.http      import JsonResponse
-from django.views     import View
-from users.models     import User
-from users.validation import validate_email, validate_password
+from django.http            import JsonResponse
+from django.views           import View
+from users.models           import User
+from users.validation       import validate_email, validate_password
+from django.core.exceptions import ValidationError
 
 class SignUpView(View):
     def post(self, request):
@@ -19,8 +20,10 @@ class SignUpView(View):
                 email        = data['email'],
                 password     = data['password'],
                 phone_number = data['phone_number'],
-                )
+            )
                     
             return JsonResponse({'message': "SUCCESS"}, status=201)
         except KeyError:
             return JsonResponse({'message': "KEY_ERROR"}, status=400)
+        except ValidationError as e:
+            return JsonResponse({'message': str(e.message)}, status=400)
