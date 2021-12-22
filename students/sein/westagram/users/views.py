a9bc04b7d2a6b356824fcafc7c8bf529ee7ebee1
seinthemag <seinthemag@gmail.com>
2021-12-22 17:57:27 +0900

    FIX: bcrypt

--- students/sein/westagram/users/views.py
+++ students/sein/westagram/users/views.py
@@ -16,21 +16,20 @@ class SignUpView(View):
             email_regex    = '[a-zA-Z0-9.-_+]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+'
             password_regex = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
-            if not re.match(email_regex, data['email']):
+            if not re.match(email_regex, email):
                 return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
 
-            if not re.match(password_regex, data['password']):
+            if not re.match(password_regex, password):
                 return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
        
             if User.objects.filter(email = data['email']).exists():
                 return JsonResponse({'message' : 'ALREADY_EXIST'}, status = 400)
 
-            hashed_password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            decoded_password = hashed_password.decode('utf-8')
-            
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+           
             User.objects.create(
                 email         = data['email'],
-                password      = decoded_password,
+                password      = hashed_password,
                 name          = data['name'],
                 mobile_number = data['mobile_number']
                 )
